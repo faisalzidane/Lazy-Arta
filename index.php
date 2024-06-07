d53c1ab97bee6d01a8b6e970d705c616cc544dab
unknown <faisalzidane07.com>
2024-06-08 04:27:05 +0700

    kuy

--- index.php
+++ index.php
@@ -1,98 +1,115 @@
-<?php 
-include 'koneksi.php';
-?>
-
 <!doctype html>
 <html lang="en">
   <head>
-
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
     <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="css/login.css">
-    <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> -->
 
-
-    <title>Halaman Login</title>
+    <title>Lazy Arta</title>
   </head>
   <body>
-  <!-- Form Login -->
-    <div class="container">
-      <h4 class="text-center">FORM LOGIN</h4>
-      <hr>
-      <form method="POST" action="">
-        <div class="form-group">
-          <label for="exampleInputEmail1">Username</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <div class="input-group-text"><i class="fas fa-user"></i></div>
-              </div>
-              <input type="text" class="form-control" placeholder="Masukkan Username" name="username">
-            </div>
+
+  <!-- Jumbotron -->
+      <div class="jumbotron jumbotron-fluid text-center">
+        <div class="container">
+          <h1 class="display-4"><span class="font-weight-bold">LAZYARTA</span></h1>
+          <hr>
+          <p class="lead font-weight-bold">Silahkan Pesan Produk Sesuai Keinginanmu <br> 
+          Enjoy Your Shopping</p>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <div class="input-group-text"><i class="fas fa-unlock-alt"></i></div>
+      </div>
+  <!-- Akhir Jumbotron -->
+
+  <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg  bg-dark">
+        <div class="container">
+        <a class="navbar-brand text-white" href="index.php"><strong>Lazy</strong> Arta</a>
+        <button class="navbar-toggler-icon bg-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="index.php">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="#">DAFTAR PRODUK</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="#">PESANAN</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="login.php">LOGIN</a>
+            </li>
+          </ul>
+        </div>
+       </div> 
+      </nav>
+  <!-- Akhir Navbar -->
+
+  <!-- Menu -->    
+      <div class="container">
+        <div class="judul text-center mt-5">
+          <h3 class="font-weight-bold">LAZYARTA</h3>
+          <h5>Komp. Bumi Parung Permai Blok C3 no.12 Kec. Parung Kab. Bogor, 16330
+          <br>Buka Jam <strong>10:00 - 17:00</strong></h5>
+        </div>
+
+        <div class="row mb-5 mt-5 ">
+          <div class="col-md-6 d-flex justify-content-end">
+            <div class="card bg-dark text-white border-light">
+              <img src="images/background/menu2.jpeg" class="card-img" alt="Lihat Daftar Menu">
+              <div class="card-img-overlay mt-5 text-center">
+               <a href="daftar_menu.php" class="btn btn-primary">LIHAT DAFTAR PRODUK</a>
               </div>
-              <input type="password" class="form-control" placeholder="Masukkan Password" name="password">
+            </div>
+          </div>
+
+          <div class="col-md-6 d-flex justify-content-start">
+            <div class="card bg-dark text-white border-light">
+              <img src="images/background/menu3.jpeg" class="card-img" alt="Lihat Pesanan">
+              <div class="card-img-overlay mt-5 text-center">
+               <a href="pesanan.php" class="btn btn-primary">LIHAT PESANAN</a>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="mb-3" >
-          <small><a href="register.php" class="text-dark">Belum Punya Akun ? Buat Akun Anda !</a></small>
+      </div>
+  <!-- Akhir Menu -->
+
+  <!-- Awal Footer -->
+      <hr class="footer">
+      <div class="container">
+        <div class="row footer-body">
+          <div class="col-md-6">
+          <div class="copyright">
+            <strong>Copyright</strong> <i class="far fa-copyright"></i> 2024 -  Designed by Kelompok E</p>
+          </div>
+          </div>
+
+          <div class="col-md-6 d-flex justify-content-end">
+          <div class="icon-contact">
+          <label class="font-weight-bold">Follow Us </label>
+          <a href="https://www.instagram.com/lazyarta?igsh=MW92YnlpM3BuOWIyNA=="><img src="images/icon/ig.png" class="mr-3" data-toggle="tooltip" title="Instagram"></a>
         </div>
-        <button type="submit" name="submit" class="btn btn-primary">LOGIN</button>
-        <button type="reset" name="reset" class="btn btn-danger">RESET</button>
-      </form>
-  <!-- Akhir Form Login -->
-
-  <!-- Eksekusi Form Login -->
-      <?php 
-        if(isset($_POST['submit'])) {
-          $user = $_POST['username'];
-          $password = $_POST['password'];
-
-          // Query untuk memilih tabel
-          $cek_data = mysqli_query($koneksi, "SELECT * FROM user WHERE username = '$user' AND password = '$password'");
-          $hasil = mysqli_fetch_array($cek_data);
-          $status = $hasil['status'];
-          $login_user = $hasil['username'];
-          $row = mysqli_num_rows($cek_data);
-
-          // Pengecekan Kondisi Login Berhasil/Tidak
-            if ($row > 0) {
-                session_start();   
-                $_SESSION['login_user'] = $login_user;
-
-                if ($status == 'admin') {
-                  header('location: admin.php');
-                }elseif ($status == 'user') {
-                  header('location: user.php'); 
-                }
-            }else{
-                header("location: index.php");
-            }
-        }
-       ?>
-    </div>
-  <!-- Akhir Eksekusi Form Login -->
-
-
-
-
-
+          </div>
+        </div>
+      </div>
+  <!-- Akhir Footer -->
 
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/jquery.js"></script>
   </body>
